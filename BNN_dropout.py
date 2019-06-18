626e602637e0daccadc8306a26c0716bd1acf6d9
Luojiaji <lt920@126.com>
2019-06-18 21:43:39 +0800

    add file

---
+++ BNN_dropout.py
@@ -0,0 +1,42 @@
+import numpy as np
+from keras.datasets import mnist
+from keras.layers import Input, Flatten, Dense, Dropout, Layer,Lambda
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
+from keras.optimizers import RMSprop, SGD
+from keras import callbacks, optimizers
+from keras.utils import np_utils
+from keras import backend as K
+from keras.models import Model
+
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = np.expand_dims(x_train, axis = 3)
+x_test = np.expand_dims(x_test, axis = 3)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+y_train = np_utils.to_categorical(y_train, num_classes=10)
+y_test = np_utils.to_categorical(y_test, num_classes=10)
+
+input_shape = (28,28,1)
+
+input_data = Input(shape=input_shape)
+x = Conv2D(32, (3, 3), activation='relu', padding='same', name='block1_conv1')(input_data)
+x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+x = Conv2D(32, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+x = Flatten(name='flatten')(x)
+x = Dense(128, activation='relu', name='fc1')(x)
+
+x = Lambda(lambda x: K.dropout(x, 0.5))(x)
+
+x = Dense(10, activation='softmax', name='fc_10')(x)
+model = Model(input_data, x)
+
+model.compile(loss = 'categorical_crossentropy', optimizer = optimizers.SGD(), metrics = ['accuracy'])
+model.summary()
+model.fit(x_train, y_train, epochs=20, batch_size=256)
+
